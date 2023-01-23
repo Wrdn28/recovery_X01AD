b8454b9a0ff6426c352272c4f93d9aeee01898a2
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2023-01-23 16:32:47 +0100

    ASUS_X01A_1: Initial TWRP device tree Made with SebaUbuntu's TWRP device tree generator Arch: arm64 Manufacturer: asus Model: ASUS_X01AD Script version: 3.0.0

---
+++ README.md
@@ -0,0 +1,10 @@
+# Android device tree for asus ASUS_X01AD (ASUS_X01A_1)
+
+```
+#
+# Copyright (C) 2023 The Android Open Source Project
+# Copyright (C) 2023 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+```
---
+++ omni_ASUS_X01A_1.mk
@@ -0,0 +1,29 @@
+#
+# Copyright (C) 2023 The Android Open Source Project
+# Copyright (C) 2023 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
+# Inherit some common Omni stuff.
+$(call inherit-product, vendor/omni/config/common.mk)
+
+# Inherit from ASUS_X01A_1 device
+$(call inherit-product, device/asus/ASUS_X01A_1/device.mk)
+
+PRODUCT_DEVICE := ASUS_X01A_1
+PRODUCT_NAME := omni_ASUS_X01A_1
+PRODUCT_BRAND := asus
+PRODUCT_MODEL := ASUS_X01AD
+PRODUCT_MANUFACTURER := asus
+
+PRODUCT_GMS_CLIENTID_BASE := android-asus-tpin
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRIVATE_BUILD_DESC="msm8953_64-user 8.1.0 OPM1 2707 release-keys"
+
+BUILD_FINGERPRINT := asus/WW_X01AD/ASUS_X01A_1:8.1.0/OPM1/15.2016.1810.126-20181116:user/release-keys
