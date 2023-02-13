8d6133e655109d71810987ae4658c69bb592db29
Wrdn28 <ifloraxd@gmail.com>
2023-02-14 02:45:13 +0700

    X01AD: bringup

--- twrp_X01AD.mk
+++ twrp_X01AD.mk
@@ -9,8 +9,8 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-# Inherit some common Omni stuff.
-$(call inherit-product, vendor/omni/config/common.mk)
+# Inherit some common twrp stuff.
+$(call inherit-product, vendor/twrp/config/common.mk)
 
 # Inherit from ASUS_X01AD device
 $(call inherit-product, device/asus/X01AD/device.mk)
